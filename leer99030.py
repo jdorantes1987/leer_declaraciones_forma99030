485545b7bb0c6bc1525734d3de3926bc967b09bb
jdorantes1987 <165170008+jdorantes1987@users.noreply.github.com>
2025-03-31 19:26:12 -0400

    Version estable, se refactorizo la funcion "extraer_informacion_declaraciones" con copilot

--- leer99030.py
+++ leer99030.py
@@ -11,105 +11,79 @@ def extraer_informacion_declaraciones():
     with open(file_path, "r", encoding="utf-8") as file:
         lineas = file.readlines()
 
-    informacion = {}
-    i = 0
-    pag = 0
-    for linea in lineas:
-        informacion[i] = linea.strip()
-        i += 1
-    periodo = ""
     informacion_fiscal = {}
     dict_periodos = {}
-    for num_linea, valores in informacion.items():
-        contenido = valores.split()
-        if (len(contenido) == 2 and "Página" not in contenido) or (
-            "FORMA" in contenido and "99030" in contenido
-        ):
+    periodo = ""
+    pag = 0
+
+    def actualizar_dict_periodos(clave, valor):
+        dict_periodos[clave] = extraer_valor(valor)
+
+    for linea in lineas:
+        contenido = linea.strip().split()
+
+        if len(contenido) == 2 and "Página" not in contenido:
             if contenido[0].isnumeric() and contenido[1].isnumeric():
                 periodo = contenido[1] + contenido[0]
-            if contenido[0] == "FORMA":
-                per_planilla = periodo + "-" + contenido[4]
-                informacion_fiscal[per_planilla] = dict_periodos
+
+        elif "FORMA" in contenido and "99030" in contenido:
+            per_planilla = periodo + "-" + contenido[4]
+            informacion_fiscal[per_planilla] = dict_periodos
 
         elif "Página" in contenido:
             pag = contenido[1]
 
         elif "Ventas" in contenido and "no" in contenido:
-            ventas_no_gravadas = {
-                "Ventas No Gravadas": extraer_valor(contenido[6]),
-            }
-            dict_periodos.update(ventas_no_gravadas)
+            actualizar_dict_periodos("Ventas No Gravadas", contenido[6])
 
         elif "Ventas" in contenido and "Gravadas" in contenido and "42" in contenido:
-            ventas_gravadas = {
-                "Ventas Base Imponible": extraer_valor(contenido[8]),
-                "Débito Fiscal": extraer_valor(contenido[10]),
-            }
-            dict_periodos.update(ventas_gravadas)
+            actualizar_dict_periodos("Ventas Base Imponible", contenido[8])
+            actualizar_dict_periodos("Débito Fiscal", contenido[10])
 
         elif "Fiscal" in contenido and "30" in contenido:
-            compras_no_gravadas = {
-                "Compras No Gravadas": extraer_valor(contenido[2]),
-            }
-            dict_periodos.update(compras_no_gravadas)
+            actualizar_dict_periodos("Compras No Gravadas", contenido[2])
 
         elif "Compras" in contenido and "Gravadas" in contenido and "33" in contenido:
-            compras_gravadas = {
-                "Compras Base Imponible": extraer_valor(contenido[8]),
-                "Crédito Fiscal": extraer_valor(contenido[10]),
-            }
-            dict_periodos.update(compras_gravadas)
+            actualizar_dict_periodos("Compras Base Imponible", contenido[8])
+            actualizar_dict_periodos("Crédito Fiscal", contenido[10])
+
         elif "Excedente" in contenido and "Fiscales" in contenido and "20" in contenido:
-            exced_cf_mes_anterior = {
-                "Exced_cf_m_Ante": extraer_valor(contenido[14]),
-            }
-            dict_periodos.update(exced_cf_mes_anterior)
+            actualizar_dict_periodos("Exced_cf_m_Ante", contenido[14])
 
         elif "Excedente" in contenido and "Fiscal" in contenido and "60" in contenido:
-            exced_cf_mes_siguiente = {
-                "Exced_cf_m_Sig": extraer_valor(contenido[10]),
-            }
-            dict_periodos.update(exced_cf_mes_siguiente)
+            actualizar_dict_periodos("Exced_cf_m_Sig", contenido[10])
 
         elif (
             "Retenciones" in contenido
             and "Acumuladas" in contenido
             and "54" in contenido
         ):
-            ret_acum = {
-                "Ret_Acum": extraer_valor(contenido[6]),
-            }
-            dict_periodos.update(ret_acum)
+            actualizar_dict_periodos("Ret_Acum", contenido[6])
 
         elif (
             "Retenciones" in contenido
             and "Descontadas" in contenido
             and "55" in contenido
         ):
-            ret_descontadas = {
-                "Ret_Desc": extraer_valor(contenido[9]),
-            }
-            dict_periodos.update(ret_descontadas)
+            actualizar_dict_periodos("Ret_Desc", contenido[9])
 
         elif (
             "Retenciones" in contenido and "Período" in contenido and "66" in contenido
         ):
-            ret_periodo = {
-                "Ret_Periodo": extraer_valor(contenido[5]),
-            }
-            dict_periodos.update(ret_periodo)
+            actualizar_dict_periodos("Ret_Periodo", contenido[5])
 
-        elif "SI" in contenido and "DECLARACIÓN" in contenido and "FECHA" in contenido:
-            if pag == "1":
-                dict_periodos.update(
-                    {"Fecha": datetime.strptime(contenido[11], "%d/%m/%Y")}
-                )
+        elif (
+            "SI" in contenido
+            and "DECLARACIÓN" in contenido
+            and "FECHA" in contenido
+            and pag == "1"
+        ):
+            dict_periodos["Fecha"] = datetime.strptime(contenido[11], "%d/%m/%Y")
 
         elif "Total" in contenido and "Pagar" in contenido and "90" in contenido:
-            dict_periodos.update({"Periodo": periodo})
+            dict_periodos["Periodo"] = periodo
             dict_periodos = {}
 
-    # convertir a dataframe
     return DataFrame(informacion_fiscal).T  # Transponer
 
 
